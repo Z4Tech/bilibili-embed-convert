403c0e05b4ee79803969dc8e45d8d39b320f0b6f
Z4Tech <zhangchenguang1994@gmail.com>
2015-09-19 19:03:16 +0800

    refined

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ var url_list = [
   'http://www.bilibili.com/video/av2182749/',
   'http://www.bilibili.com/video/av1032809/',
   'http://www.bilibili.com/video/av2053/'
-]
+];
 
 var flash_list = [
   'http://share.acg.tv/flash.swf?aid=2333333&page=3',
@@ -24,7 +24,7 @@ var flash_list = [
   'http://share.acg.tv/flash.swf?aid=2182749&page=1',
   'http://share.acg.tv/flash.swf?aid=1032809&page=1',
   'http://share.acg.tv/flash.swf?aid=2053&page=1'
-]
+];
 
 var embed_list = [
   '<embed height="452" width="544" quality="high" allowfullscreen="true" type="application/x-shockwave-flash" src="http://share.acg.tv/flash.swf" flashvars="aid=2333333&page=3" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash"></embed>',
@@ -37,7 +37,7 @@ var embed_list = [
   '<embed height="452" width="544" quality="high" allowfullscreen="true" type="application/x-shockwave-flash" src="http://share.acg.tv/flash.swf" flashvars="aid=2182749&page=1" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash"></embed>',
   '<embed height="452" width="544" quality="high" allowfullscreen="true" type="application/x-shockwave-flash" src="http://share.acg.tv/flash.swf" flashvars="aid=1032809&page=1" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash"></embed>',
   '<embed height="452" width="544" quality="high" allowfullscreen="true" type="application/x-shockwave-flash" src="http://share.acg.tv/flash.swf" flashvars="aid=2053&page=1" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash"></embed>'
-]
+];
 
 console.log("Manual Tests");
 var bili_video_1 = new Bili_video(1, 1);
@@ -46,18 +46,18 @@ var bili_video_2 = new Bili_video(2);
 console.log(bili_video_2.getIndex());
 
 console.log("Auto Tests");
-for(i in url_list){
+for(var i in url_list){
   console.log(url_list[i]);
   var bili_video = new Bili_video(url_list[i]);
   console.log(bili_video.avnumber + ' ' + bili_video.page);
-  if(bili_video.flashAddr() == flash_list[i]){
+  if(bili_video.flashAddr() === flash_list[i]){
     console.log("Correct: " + flash_list[i]);
   }else{
     console.error("Error!!!");
     console.error(bili_video.flashAddr());
     console.error(url_list[i]);
   }
-  if(bili_video.embedAddr() == embed_list[i]){
+  if(bili_video.embedAddr() === embed_list[i]){
     console.log("Correct: " + embed_list[i]);
   }else {
     console.error("Error!!!");
